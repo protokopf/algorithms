74a1b12fe3ce9103e94d3f83ddcd3a47aa389147
Konstantin Kovalenko <protokopf@gmail.com>
2018-03-27 23:16:28 +0300

    - Added solution for AddTwoNumber problem.

---
+++ Algorythms.Solution/Algorythms.Solution/LeetCode/AddTwoNumbersProblem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorythms.Solution.LeetCode
+{
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+    }
+
+    public static class NodeSolution
+    {
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode firstNode = null;
+            ListNode currentNode = null;
+            int diff = 0;
+            while(!(l1 == null && l2 == null && diff != 1))
+            {
+                int sumOfTwo = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + diff;
+                if(sumOfTwo >= 10)
+                {
+                    diff = 1;
+                    sumOfTwo -= 10;
+                }
+                else
+                {
+                    diff = 0;
+                }
+                
+                if (firstNode == null)
+                {
+                    firstNode = currentNode = new ListNode(sumOfTwo);
+                }
+                else
+                {
+                    ListNode newNode = new ListNode(sumOfTwo);
+                    currentNode.next = newNode;
+                    currentNode = newNode;
+                }
+                l1 = l1 != null ? l1.next : null;
+                l2 = l2 != null ? l2.next : null;
+            }
+            return firstNode;
+        }
+    }
+
+    public static class Helpers
+    {
+        public static ListNode CreateNodeFromArray(int[] array)
+        {
+            int length = array.Length;
+            if(length == 0)
+            {
+                return new ListNode(0);
+            }
+            ListNode first = new ListNode(array[0]);
+            ListNode current = first;
+            for(int i = 1; i < length; ++i)
+            {
+                ListNode newNode = new ListNode(array[i]);
+                current.next = newNode;
+                current = newNode;
+            }
+
+            return first;
+        }
+    }
+}
--- Algorythms.Solution/Algorythms.Solution/Program.cs
+++ Algorythms.Solution/Algorythms.Solution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Algorythms.Solution.BuildAndAnalysis._2._1;
+using Algorythms.Solution.LeetCode;
 using System;
 
 namespace Algorythms.Solution
@@ -8,9 +9,24 @@ namespace Algorythms.Solution
         static void Main(string[] args)
         {
             //2.1 Insertion sort
-            int[] initialArray = new int[] { 9, 5, 10, 3, 7, 6, 2, 1, 4, 8 };
-            initialArray.InsertionSort((a, b) => a < b);
-            PrintArray(initialArray);
+
+            //int[] initialArray = new int[] { 9, 5, 10, 3, 7, 6, 2, 1, 4, 8 };
+            //initialArray.InsertionSort((a, b) => a < b);
+            //PrintArray(initialArray);
+
+            //LeetCode AddTwoNumbers problem
+            ListNode l1 = Helpers.CreateNodeFromArray(new[] { 2, 4, 3 });
+            ListNode l2 = Helpers.CreateNodeFromArray(new[] { 5, 6, 4 });
+            ListNode answer = NodeSolution.AddTwoNumbers(l1, l2);
+
+            ListNode l11 = Helpers.CreateNodeFromArray(new[] { 0 });
+            ListNode l22 = Helpers.CreateNodeFromArray(new[] { 0 });
+            ListNode answer2 = NodeSolution.AddTwoNumbers(l11, l22);
+
+            ListNode l111 = Helpers.CreateNodeFromArray(new[] { 9 });
+            ListNode l222 = Helpers.CreateNodeFromArray(new[] {1,9,9,9,9,9,9,9,9,9});
+            ListNode answer22 = NodeSolution.AddTwoNumbers(l111, l222);
+
 
             Console.Read();
         }
