29b4ed54919c652f7c8af6784bfa639c4af87e09
Konstantin Kovalenko <protokopf@gmail.com>
2018-03-27 21:16:43 +0300

    - Added Insertion sort algorithms. Tested it in console.

---
+++ Algorythms.Solution/Algorythms.Solution/BuildAndAnalysis/2.1/Algorithms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorythms.Solution.BuildAndAnalysis._2._1
+{
+    public static class Algorithms
+    {
+        public static void InsertionSort<T>(this T[] array, Func<T,T, bool> comparer)
+        {
+            int length = array.Length;
+            for(int i = 0; i < length; ++i)
+            {
+                T currentValue = array[i];
+                int j = i - 1;
+                while(j >= 0 && comparer(currentValue, array[j]))
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = currentValue;
+            }
+        }
+    }
+}
--- Algorythms.Solution/Algorythms.Solution/Program.cs
+++ Algorythms.Solution/Algorythms.Solution/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Algorythms.Solution.BuildAndAnalysis._2._1;
+using System;
 
 namespace Algorythms.Solution
 {
@@ -10,6 +7,23 @@ namespace Algorythms.Solution
     {
         static void Main(string[] args)
         {
+            //2.1 Insertion sort
+            int[] initialArray = new int[] { 9, 5, 10, 3, 7, 6, 2, 1, 4, 8 };
+            initialArray.InsertionSort((a, b) => a < b);
+            PrintArray(initialArray);
+
+            Console.Read();
+        }
+
+        private static void PrintArray<T>(T[] array)
+        {
+            Console.WriteLine("Printing array!");
+            int length = array.Length;
+            for(int i = 0; i < length; ++i)
+            {
+                Console.WriteLine($"--[{i}] = {array[i]}");
+            }
+            Console.WriteLine("Finished printing array!");
         }
     }
 }
