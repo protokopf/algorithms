69b0a5b6649bbaa0eebf9a4e50bb827e4104caef
Konstantin Kovalenko <protokopf@gmail.com>
2018-04-01 13:38:48 +0300

    - Added LongestSubstring algorithms and covered it with unit tests.

---
+++ Algorythms.Solution/Algorithms.Test/LeetCode/LongestSubstring.Test.cs
@@ -0,0 +1,27 @@
+﻿using Algorythms.Solution.LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test.LeetCode
+{
+    [TestFixture]
+    class LongestSubstringTest
+    {
+        [TestCase("abcav", 4)]
+        [TestCase("abcavea", 5)]
+        public void TestAlgorithm(string str, int length)
+        {
+            //Arrange
+
+            //Act
+            int actualLength = LongestSubstring.FindLongestSubstring(str);
+
+            //Assert
+            Assert.AreEqual(length, actualLength);
+        }
+    }
+}
---
+++ Algorythms.Solution/Algorythms.Solution/LeetCode/LongestSubstring.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorythms.Solution.LeetCode
+{
+    public static class LongestSubstring
+    {
+        public static int FindLongestSubstring(string s)
+        {
+            int length = s.Length;
+            int max = 0;
+            for (int i = 0; i < length; ++i)
+            {
+                if (length - i < max)
+                {
+                    break;
+                }
+                HashSet<char> hash = new HashSet<char>();
+                for (int j = i; j < length; ++j)
+                {
+                    if (!hash.Add(s[j]))
+                    {
+                        break;
+                    }
+                }
+                int hashCount = hash.Count;
+                max = max > hashCount ? max : hashCount;
+            }
+            return max;
+        }
+    }
+}
